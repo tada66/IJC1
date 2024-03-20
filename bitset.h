c32996ad2e191a3f0cbded964dd2e84c940828cc
Tadeas Horak <horaktada@seznam.cz>
2024-03-20 21:24:17 +0100

    mostly working bitset_alloc

--- bitset.h
+++ bitset.h
@@ -1,11 +1,6 @@
 #include <stdbool.h>
 #include <assert.h>
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <limits.h>
-#include "error.h"
-
 typedef struct {
 }bitset_t;
 
@@ -15,7 +10,14 @@ typedef unsigned long bitset_index_t;
 
 #define bitset_create(name,size)                            \
     static_assert(size > 0);                                \
-    printf("%ld\n", bitset_index_bits(size));               \
+    printf("bitset_create: bytes=%ld\n", bitset_index_bits(size));               \
     bitset_index_t name[bitset_index_bits(size)]= {size}
 
+#define bitset_alloc(name, size)                            \
+    assert(size > 0);                                \
+    printf("bitset_alloc: bytes=%ld\n", bitset_index_bits(size));               \
+    bitset_index_t* name = calloc(bitset_index_bits(size), sizeof(unsigned long));   \
+    if(&name == NULL)\
+        fprintf(stderr, "bitset_alloc: Chyba alokace paměti");  /*TODO EXIT APP !!*/   \
+    name[0] = size
 
--- main.c
+++ main.c
@@ -3,10 +3,17 @@
 #include "bitset.h"
 
 void main(){
-    bitset_create(bruhpole, 100);
-    printf("Size of arr=%ld\n", sizeof(bruhpole)*8);
-    for(int i=0; i<sizeof(bruhpole)/sizeof(unsigned long); i++){
-        printf("%ld, ", bruhpole[i]);
+    bitset_create(cpole, 128);
+    printf("Size of arr=%ld\n", sizeof(cpole)*8);
+    for(int i=0; i<sizeof(cpole)/sizeof(unsigned long); i++){
+        printf("%ld, ", cpole[i]);
+    }
+    printf("\n\n\n");
+
+    bitset_alloc(apole, 128);
+    printf("Size of arr=%ld\n", sizeof(apole)*8);
+    for(int i=0; i<sizeof(apole)/sizeof(unsigned long); i++){
+        printf("%ld, ", apole[i]);
     }
     printf("\n");
 }
