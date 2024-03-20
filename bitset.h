05f452d915edc739b892ed37be7420a8f3cad255
Tadeas Horak <horaktada@seznam.cz>
2024-03-20 20:23:26 +0100

    working bitset create

--- bitset.h
+++ bitset.h
@@ -11,9 +11,11 @@ typedef struct {
 
 typedef unsigned long bitset_index_t;
 
+#define bitset_index_bits(count) (count-1+16*sizeof(unsigned long))/sizeof(unsigned long)/8 
 
-#define bitset_create(name,size)        \
-    static_assert(size > 0);            \
-    bitset_index_t name[(size-1+2*sizeof(unsigned long))/sizeof(unsigned long)]= {size}
+#define bitset_create(name,size)                            \
+    static_assert(size > 0);                                \
+    printf("%ld\n", bitset_index_bits(size));               \
+    bitset_index_t name[bitset_index_bits(size)]= {size}
 
 
--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 #include "bitset.h"
 
 void main(){
-    bitset_create(bruhpole, 50);
-    printf("Size of arr=%ld\n", sizeof(bruhpole));
+    bitset_create(bruhpole, 100);
+    printf("Size of arr=%ld\n", sizeof(bruhpole)*8);
     for(int i=0; i<sizeof(bruhpole)/sizeof(unsigned long); i++){
         printf("%ld, ", bruhpole[i]);
     }
