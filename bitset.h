9f3217d2a83fde67c40ef52be21e598eb4e05680
Tadeas Horak <horaktada@seznam.cz>
2024-03-20 21:56:35 +0100

    working fill

--- bitset.h
+++ bitset.h
@@ -17,7 +17,21 @@ typedef unsigned long bitset_index_t;
     assert(size > 0);                                \
     printf("bitset_alloc: bytes=%ld\n", bitset_index_bits(size));               \
     bitset_index_t* name = calloc(bitset_index_bits(size), sizeof(unsigned long));   \
-    if(&name == NULL)\
+    if(&name == NULL){\
         fprintf(stderr, "bitset_alloc: Chyba alokace paměti");  /*TODO EXIT APP !!*/   \
+        exit(1);\
+    }\
     name[0] = size
 
+#define bitset_free(name)\
+    free(&name);
+
+#define bitset_size(name)\
+    name[0]
+
+#define bitset_fill(name, set)\
+    for(int i=0; i<bitset_size(name)/sizeof(unsigned long)/8; i++)\
+        if(set)\
+            name[i+1]=1;\
+        else\
+            name[i+1]=0
--- main.c
+++ main.c
@@ -5,14 +5,19 @@
 void main(){
     bitset_create(cpole, 128);
     printf("Size of arr=%ld\n", sizeof(cpole)*8);
-    for(int i=0; i<sizeof(cpole)/sizeof(unsigned long); i++){
+    printf("declared=%ld\n", bitset_size(cpole));
+    bitset_fill(cpole, true);
+    for(int i=0; i<bitset_size(cpole)/sizeof(unsigned long)/8+1; i++){
         printf("%ld, ", cpole[i]);
     }
+    printf("\n");
     printf("\n\n\n");
 
     bitset_alloc(apole, 128);
     printf("Size of arr=%ld\n", sizeof(apole)*8);
-    for(int i=0; i<sizeof(apole)/sizeof(unsigned long); i++){
+    printf("declared=%ld\n", bitset_size(apole));
+    bitset_fill(apole, true);
+    for(int i=0; i<bitset_size(apole)/sizeof(unsigned long)/8+1; i++){
         printf("%ld, ", apole[i]);
     }
     printf("\n");
