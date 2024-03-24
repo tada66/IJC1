1cd7d3ae3b206c40d3982b7e8b7a4e3e67116096
Tadeas Horak <horaktada@seznam.cz>
2024-03-24 15:03:26 +0100

    added comments

--- bitset.h
+++ bitset.h
@@ -17,10 +17,12 @@ typedef unsigned long bitset_index_t;
 
 #define bitset_index_bits(count) (count-1+16*sizeof(unsigned long))/sizeof(unsigned long)/8 
 
+//Create the bit array in stack
 #define bitset_create(name,size)                            \
     static_assert(size > 0);                                \
     bitset_index_t name[bitset_index_bits(size)]= {size}
 
+//Allocate memory in heap and create the bit array 
 #define bitset_alloc(name, size)                            \
     assert(size > 0);                                \
     bitset_t name = calloc(bitset_index_bits(size), sizeof(unsigned long));   \
@@ -30,14 +32,18 @@ typedef unsigned long bitset_index_t;
     }\
     name[0] = size
 
-#ifndef USE_INLINE
+//Create and alloc cannot be done by inline functions, however, the others can
+#ifndef USE_INLINE  //If USE_INLINE is not set by -DUSE_INLINE flag we use macros
 
+//frees the memory allocated by bitset_alloc
 #define bitset_free(name)\
     free(&name);
 
+//returns the requested size of the array
 #define bitset_size(name)\
     name[0]
 
+//Fills the whole array with 0s or 1s depending on 'set' (true=1s)
 #define bitset_fill(name, set)\
     for(unsigned i=0; i<(bitset_size(name)+(sizeof(unsigned long)*8)-1)/sizeof(unsigned long)/8; i++)\
     {\
@@ -47,6 +53,7 @@ typedef unsigned long bitset_index_t;
             name[i+1]=0;\
     }
 
+//Set a single bit of the array to either 1 or 0
 #define bitset_setbit(name, index, set)\
     do{\
     if(index>=bitset_size(name))\
@@ -65,21 +72,24 @@ typedef unsigned long bitset_index_t;
     name[index/(sizeof(unsigned long)*8)+1] = num;\
     } while (0)    
 
+//Get the value of a single bit in the array
 #define bitset_getbit(name, index)\
     (index>=bitset_size(name)) ? \
     (error_exit("bitset_getbit: Index %lu mimo rozsah 0..%lu",(unsigned long)index, bitset_size(name)-1),0) : \
     ((name[index / (sizeof(unsigned long) * 8)+1] >> (index % (sizeof(unsigned long) * 8))) & 1)
 
 #else
-
+    //frees the memory allocated by bitset_alloc
     inline void bitset_free(bitset_index_t *name){
         free(name);
     }
 
+    //returns the requested size of the array
     inline unsigned long bitset_size(bitset_index_t *name){
         return name[0];
     }
 
+    //Fills the whole array with 0s or 1s depending on 'set' (true=1s)
     inline void bitset_fill(bitset_index_t *name, bool set){
     for(unsigned i=0; i<(bitset_size(name)+(sizeof(unsigned long)*8)-1)/sizeof(unsigned long)/8; i++)
         if(set)
@@ -88,6 +98,7 @@ typedef unsigned long bitset_index_t;
             name[i+1]=0;
     }
 
+    //Set a single bit of the array to either 1 or 0
     inline void bitset_setbit(bitset_index_t *name, unsigned long index, bool set){
         if(index>=bitset_size(name))
             error_exit("bitset_setbit: Index %lu mimo rozsah 0..%lu",(unsigned long)index, bitset_size(name)-1);\
@@ -105,6 +116,7 @@ typedef unsigned long bitset_index_t;
         name[index/(sizeof(unsigned long)*8)+1] = num;
     }
 
+    //Get the value of a single bit in the array
     inline unsigned long bitset_getbit(bitset_index_t *name, unsigned long index){
         return (index>=bitset_size(name) && index<1) ? 
         (error_exit("bitset_getbit: Index %lu mimo rozsah 0..%lu",(unsigned long)index, bitset_size(name)-1),0) :
